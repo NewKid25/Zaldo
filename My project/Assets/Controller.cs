22dafa5bbdb40957a25ede9b9936deffa8e80b03
NewKid25 <115599059+NewKid25@users.noreply.github.com>
2022-12-21 14:49:13 -0600

    movement and camera set up
    fine tuning still to be done

--- My project/Assets/Controller.cs
+++ My project/Assets/Controller.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator animator;
 
     private float horizontal;
     private bool touch;
+    private float sqrSpeedCap;
+    private bool isFlipped;
 
     public float jumpForce = 20f;
-    public float speed = 20f;
+    public float moveForce = 20f;
+    public float speedCap = 30f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        sqrSpeedCap = speedCap * speedCap;
     }
 
     // Update is called once per frame
@@ -32,11 +40,26 @@ public class Controller : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.AddForce(Vector2.right * (horizontal * speed));
+        rb.AddForce(Vector2.right * (horizontal * moveForce));
+
+        if (horizontal < 0 && isFlipped == false)
+        {
+            Flip();
+        }
+        else if (horizontal > 0 && isFlipped == true)
+        {
+            Flip();
+        }
+        //gameObject.transform.position += new Vector3(horizontal * speed,0f, 0f);
+
+        if (rb.velocity.sqrMagnitude > sqrSpeedCap)
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, speedCap);
+
+        Debug.Log(rb.velocity.sqrMagnitude);
     }
 
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground")) 
         touch = true;
@@ -49,7 +72,17 @@ public class Controller : MonoBehaviour
 
     private void Jump()
     {
-        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+        //gameObject.transform.position += new Vector3(0f, jumpForce, 0f);
+
+        animator.Play("Player_Jump");
+    }
+
+    void Flip()
+    {
+        gameObject.transform.Rotate(0f,180f, 0f);
+        isFlipped = !isFlipped;
     }
 
 }
